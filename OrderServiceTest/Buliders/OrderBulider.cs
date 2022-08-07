cd74b80420adb368ece89355c6e527d9e7b430e2
Faezeh Nozari <faezeh.n31@gmail.com>
2022-08-07 15:08:41 +0430

    Craete Builder Pattern For Test

---
+++ OrderServiceTest/Buliders/OrderBulider.cs
@@ -0,0 +1,44 @@
+﻿using OrderService;
+using OrderServiceTest.Buliders;
+using System.Collections.Generic;
+
+namespace OrderServiceTest.Bulider
+{
+    public class OrderBulider
+    {
+		private int _userId;
+		private List<OrderItem> _orderItems = new();
+		OrderItem orderItem = new OrderItemBuilder().Build();
+
+        public OrderBulider()
+		{
+			_userId = 12;
+			_orderItems.Add(orderItem);
+		}
+
+
+		public OrderBulider WithUserId(int userId)
+		{
+			_userId = userId;
+			return this;
+		}
+
+		public OrderBulider WithOrderItems(List<OrderItem> orderItems)
+		{
+			_orderItems = orderItems;
+			return this;
+		}
+
+		public OrderBulider WithOrderItem(OrderItem orderItem)
+		{
+			this.orderItem = orderItem;
+			return this;
+		}
+
+		public Order Build()
+		{
+			var order = new Order(_userId, _orderItems);
+			return order;
+		}
+	}
+}
---
+++ OrderServiceTest/Buliders/OrderItemBuilder.cs
@@ -0,0 +1,35 @@
+﻿using OrderService;
+
+namespace OrderServiceTest.Buliders
+{
+    public class OrderItemBuilder
+    {
+		private int _count;
+		private string _name;
+
+		public OrderItemBuilder()
+		{
+			_count = 1;
+			_name = "Speaker";
+		}
+
+
+		public OrderItemBuilder WithCount(int count)
+		{
+			_count = count;
+			return this;
+		}
+
+		public OrderItemBuilder WithName(string name)
+		{
+			_name = name;
+			return this;
+		}
+
+		public OrderItem Build()
+		{
+			var orderItem = new OrderItem(_count, _name);
+			return orderItem;
+		}
+	}
+}
